ef9211eda3e63af428f77dc4d02274732878f981
user <user@dev-android>
2016-09-27 09:30:20 +0800

    edit

--- src/main/java/com/wichat/controller/LoginController.java
+++ src/main/java/com/wichat/controller/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.wichat.controller.base.BaseController;
-import com.wichat.pojo.WichatUser;
 
 @Controller
 @RequestMapping(value="/admin")
